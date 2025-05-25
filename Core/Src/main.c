806ba0301bb124f242dfd3ac154292e6c7158cb3
AH-Bagheri <whitewolf97@yahoo.com>
2025-05-25 14:52:34 +0330

    SD Card is working... HAL SPI Driver + BareMetal Embedded C

---
+++ Core/Src/main.c
@@ -0,0 +1,126 @@
+
+#include "main.h"
+#include "fatfs.h"
+#include "spi.h"
+#include "usart.h"
+#include "gpio.h"
+#include "string.h"
+
+FATFS fs;
+    FIL file;
+    FRESULT res;
+    UINT bw;
+
+
+void uart_log(const char *msg) {
+    HAL_UART_Transmit(&huart2, (uint8_t *)msg, strlen(msg), HAL_MAX_DELAY);
+}
+
+void SystemClock_Config(void);
+
+int main (void) {
+
+  HAL_Init();
+  SystemClock_Config();
+  MX_GPIO_Init();
+  MX_SPI1_Init();
+  MX_USART2_UART_Init();
+  MX_FATFS_Init();
+  
+   // FATFS variables
+    
+    uart_log("Hello\n");
+    HAL_Delay(500);
+    // Mount SD Card
+    res = f_mount(&fs, "", 1);
+    if (res == FR_OK) {
+        uart_log("SD card mounted successfully.\r\n");
+
+        // Open or create log.txt for writing
+        res = f_open(&file, "log.txt", FA_WRITE | FA_OPEN_ALWAYS);
+        if (res == FR_OK) {
+            // Move file pointer to the end for appending
+            f_lseek(&file, f_size(&file));
+
+            char log_entry[] = "Write to sd card\n";
+
+            res = f_write(&file, log_entry, strlen(log_entry), &bw);
+            if (res == FR_OK && bw == strlen(log_entry)) {
+                uart_log("Data written to log.txt\r\n");
+            } else {
+                uart_log("Failed to write data\r\n");
+            }
+
+            f_close(&file);
+        } else {
+            uart_log("Failed to open log.txt\r\n");
+        }
+
+        f_mount(NULL, "", 1); // Unmount SD
+    } else {
+        uart_log("Failed to mount SD card\r\n");
+    }
+
+  
+  while (1) {
+    __NOP();
+  }
+  
+}
+
+
+void SystemClock_Config (void) {
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 25;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Enables the Clock Security System
+  */
+  HAL_RCC_EnableCSS();
+}
+
+
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
